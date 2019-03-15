59d90e84bf43f32f87ef2ac431a5c212d795da8c
Mariusz Mycek <m.mycek83@gmail.com>
2019-03-15 17:41:30 +0100

    Change syntax to JSX

---
+++ components/Contact.js
@@ -0,0 +1,24 @@
+var Contact = React.createClass({
+  propTypes: {
+    item: React.PropTypes.object.isRequired
+  },
+  render: function() {
+    return (
+      <div className="contactItem">
+        <img
+          className="contactImage"
+          src={
+            "http://icons.veryicon.com/256/System/Dynamic%20Yosemite/Contacts.png"
+          }
+        />
+        <div className="label-wrapper">
+          <p className="contactLabel">Imię: {this.props.item.firstName}</p>
+          <p className="contactLabel">Nazwisko: {this.props.item.lastName}</p>
+          <a href={"mailto: " + this.props.item.email} className="contactEmail">
+            {this.props.item.email}
+          </a>
+        </div>
+      </div>
+    );
+  }
+});
